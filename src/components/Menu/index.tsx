13c62862ff207362572a47912a3113e0d6cf9e11
Mardem Paz <mardenexecutive@gmail.com>
2025-06-18 15:26:29 -0300

    feat: initial project setup.

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,16 @@
+import './Menu.module.css';
+
+function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import "./Dashboard.module.css";
+import Menu from "../../components/Menu";
+
+function Dashboard() {
+  return (
+    <>
+      <Menu />
+      <h1>Olá Mundo!</h1>
+    </>
+  );
+}
+
+export default Dashboard;
